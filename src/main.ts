2cd53b2d6d64c391a96ade173333b24c13b295aa
mie035 <hello@michitoshiyano.com>
2024-09-16 23:15:58 +0900

    mod

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,40 @@
 import { GraphAI } from "graphai";
-import * as agents from "@graphai/vanilla";
+import * as agents from "@graphai/agents";
 
-const graph_data = {
+const graphData = {
   version: 0.5,
   nodes: {
-    node1: {
-      value: "hello, GraphAI",
+    inputData: {
+      value: "hello, let me know the answer 1 + 1",
     },
-    node2: {
-      agent: "bypassAgent",
-      inputs: [":node1"],
-      isResult: true,
+    llm: {
+      agent: "openAIAgent",
+      inputs: {
+        prompt: ":inputData",
+      },
+      params: {
+        baseURL: "http://127.0.0.1:11434/v1",
+        model: "llama3.1",
+        apiKey: "llama3.1",
+      },
     },
+    output: {
+        agent: "stringTemplateAgent",
+        params: {
+          template: "\\e[32m${1}:\\e[0m ${0}\\n",
+        },
+        console: {
+          after: true,
+        },
+        inputs: [":llm.choices.$0.message.content"],
+      },
   },
 };
 
 export const main = async () => {
-  const graph = new GraphAI(graph_data, agents);
+  const graph = new GraphAI(graphData, agents);
   const result = await graph.run();
   console.log(JSON.stringify(result));
-
 };
 
 if (process.argv[1] === __filename) {
