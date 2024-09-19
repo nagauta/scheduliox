40210f593351d81f7f9f840bfd71b2d72def921e
mie035 <hello@michitoshiyano.com>
2024-09-20 00:19:52 +0900

    mod

--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,66 @@ const graphData = {
   version: 0.5,
   nodes: {
     inputData: {
-      value: "Please arrange a meeting for Mike and Rei.Mike has 2024/09/22 and 2024/09/23 available.Rei has 2024/09/23 and 2024/09/24 available.",
+      value:
+        "Please arrange a meeting for Mike and Rei.",
+    },
+    mikeCalenders: {
+      value: " 2024/09/22 13:00 - 2024/09/22 21:00 and 2024/09/23 15:00 - 2024/09/23 19:00",
+    },
+    reiCalenders: {
+      value: " 2024/09/23 13:00 - 2024/09/23 21:00 and 2024/09/24 13:00 - 2024/09/23 21:00",
+    },
+    mikeAgent: {
+      agent: "stringTemplateAgent",
+      inputs: [":mikeCalenders"],
+      params: {
+        template: "Mike is available on ${0}",
+      },
+      console:{
+        after: true,
+      }
+    },
+    reiAgent: {
+      agent: "stringTemplateAgent",
+      inputs: [":reiCalenders"],
+      params: {
+        template: "Rei is available on ${0}",
+      },
+      console:{
+        after: true,
+      }
+    },
+    arrengerX: {
+      agent: "stringTemplateAgent",
+      inputs: [":mikeAgent", ":reiAgent"],
+      params: {
+        template: "${0} and ${1}.\n Please suggest 3 meeting slot in 1 hour.",
+      },
+      console:{
+        after: true,
+      }
     },
     llm: {
       agent: "openAIAgent",
       inputs: {
-        prompt: ":inputData",
+        prompt: ":arrengerX",
       },
       params: {
-        baseURL: "http://127.0.0.1:11434/v1",
-        model: "llama3.1",
-        apiKey: "llama3.1",
+        model: "gpt-4o",
+        // baseURL: "http://127.0.0.1:11434/v1",
+        // apiKey: "llama3.1",
       },
     },
     output: {
-        agent: "stringTemplateAgent",
-        params: {
-          template: "\\e[32m${1}:\\e[0m ${0}\\n",
-        },
-        console: {
-          after: true,
-        },
-        inputs: [":llm.choices.$0.message.content"],
+      agent: "stringTemplateAgent",
+      params: {
+        template: "\e[32mLlama3\e[0m: ${0}"
       },
+      console: {
+        after: true,
+      },
+      inputs: [":llm.choices.$0.message.content"],
+    },
   },
 };
 
